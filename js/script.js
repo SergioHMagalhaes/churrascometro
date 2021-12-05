0669b62f396330bdb3ea0035b4342987917d385e
SergioHMagalhaes <ssergioh3@gmail.com>
2021-12-05 17:54:36 -0300

    :bug: Corrigindo alguns bugs

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let oxMeat = 0
 let meat = 0
 
+
 function calculateMeat(p = people[0],t = 400){
     const total = p * t
     const totalBeef = (total * 60) / 100
@@ -26,6 +27,7 @@ function result(){
     const people = [inputMen.value, inputWoman.value, inputChild.value]
     const totalPeople = Number(people[0]) + Number(people[1]) + Number(people[2])
 
+    
     //carnes
     let checkBeef = document.getElementsByClassName('checkBeef')
     let checkMeat = document.getElementsByClassName('checkMeat')
@@ -52,6 +54,10 @@ function result(){
     }
     console.log(namesBeef, namesMeat)
 
+    if (totalPeople <= 0 || oxMeat + meat == 0){
+        alert('Informe a quantidade de pessoas e as carnes para o churras!!!')
+    }
+    else{
     //cálculo
     const amountOfMeatMen = calculateMeat(people[0],400)
     const amountOfBeefWoman = calculateMeat(people[1],300)
@@ -87,8 +93,29 @@ function result(){
     
     sectionResult.style.display = 'block'
     information.style.display = 'none'
+    }
+    
 }
 
 function reload(){
     document.location.reload(true)
+}
+
+
+const changeColor = () => {
+    let button1 = document.getElementsByClassName('button1')
+
+    button1[0].style.backgroundImage = 'linear-gradient(45deg,#FF8618, #C93FAE)'
+    button1[1].style.backgroundImage = 'linear-gradient(45deg,#FF8618, #C93FAE)'
+    button1[0].style.color = '#FFF'
+    button1[1].style.color = '#FFF'
+
+    button1[0].addEventListener('mouseout', end)
+    button1[1].addEventListener('mouseout', end)
+    function end (){
+        button1[0].style.background = '#FFF'
+        button1[1].style.background = '#FFF'
+        button1[0].style.color = '#313232'
+        button1[1].style.color = '#313232'
+    }
 }
